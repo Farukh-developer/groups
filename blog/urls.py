33d39c60d5f44c651eda95d493a1857932a623c3
farukh <farukh@farukhs-MacBook-Pro.local>
2024-06-01 12:02:53 -0700

    Profile_2

--- blog/urls.py
+++ blog/urls.py
@@ -7,29 +7,20 @@ app_name = 'users'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+   
+   
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('edit-profile/', EditProfileView.as_view(), name='edit_profile'),
    
+    
     path('group/', GroupsView.as_view(), name='group'),
     path('create/', create, name='create'),
 
+   
     path('read/<int:id>/', read, name='read'),
     path('update/<int:id>/', update, name='update'),
     path('delete/<int:id>/', delete, name='delete'),
-
-##########
-#    path('', LoginView.as_view(), name='login'),
-#     path('register/', RegisterView.as_view(), name='register'),
-#     path('logout/', LogautView.as_view(), name='logout'),
-#     path('create/', Create.as_view(), name='create'),
-#     path('profil/<int:id>/', ProfileView.as_view(), name='profil'),
-#     path('edit/<int:id>/', EditProfileView.as_view(), name='edit'),
-#     path('delete/<int:id>/', delete, name='delete'),
-#     path('students/', TableView.as_view(), name='students'),
-    
-    
-    
     
     
 ]
